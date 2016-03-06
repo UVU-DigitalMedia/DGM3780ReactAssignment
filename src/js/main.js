8be7d5c0bedd967e26a9a7633fdbb1f91c7af320
Braydon Powell <thetic1@gmail.com>
2016-03-06 10:19:35 -0700

    Now using State instead of re rendering the entire dom again

--- src/js/main.js
+++ src/js/main.js
@@ -10,47 +10,37 @@ var ReactDOM = require('react-dom');
 ///////////////////////////////////////////////////////////////
 ///      Edit below. All JS you need is included above.     ///
 ///////////////////////////////////////////////////////////////
-
 var list = [{ key: 0, task:'Finish Time Machine', category: 'regular' }, { key: 1, task: 'Realign Spectral Anamoly Cannon', category: 'regular' }, { key: 2, task: 'Explain Dark Matter to Steven Hawking', category: 'urgent' }, { key: 3, task: 'Do the dishes', category: 'long-term' }];
-    
+
 var TodoList = React.createClass({
-
-  getInitialState: function () {
-  
-    return { changed: false };
-  
+    
+  getInitialState: function() {
+      
+      return {list : this.props.list}
   },
-
+    
+  handleClick: function() {
+        
+    this.state.list.push({key: list.length, task: document.getElementById('item-INPUT').value, category: document.getElementById('categorySelect').value });
+    this.setState(this.state);
+      
+  },
+    
   render: function() {
+   
     return (
-    <div className="categoryList">
-        <TodoInput list={this.props.list}/>
-        <TodoItems list={this.props.list}/>
-    </div>
+        <div className="categoryList">
+            <TodoInput click={this.handleClick}/>
+            <TodoItems list={this.state.list}/>
+        </div>
     );
   }
-});    
-    
-var TodoInput = React.createClass({
+});
 
-  handleClick: function(e) {
-    
-        e.preventDefault();
+var TodoInput = React.createClass({
         
-        list.push({key: list.length, task: $('#item-INPUT').val(), category: $('#categorySelect').val() });
-        
-        console.log(list);
-        
-        ReactDOM.render(
-            <TodoList list={list}/>,
-            document.getElementById('content')
-        );
-        
-  },
-    
   render: function() {
     return (
-        
         <div className="newToDoForm">
             <label for="item-INPUT">TODO Item:</label>
             <input type="text" id="item-INPUT" />
@@ -62,13 +52,14 @@ var TodoInput = React.createClass({
                 <option value="long-term">Long Term</option>
             </select>
             
-            <button onClick={this.handleClick} type="button" id="item-SUBMIT" class="btn btn-sm btn-default">Submit</button>
+            <button onClick={this.props.click} type="button" id="item-SUBMIT" class="btn btn-sm btn-default">Submit</button>
         </div>       
     );
   }
-});    
+});
 
 var TodoItems = React.createClass({
+
   render: function() {
   
   var items = this.props.list.map(function(item) {
@@ -85,10 +76,12 @@ var TodoItems = React.createClass({
 });  
     
 var TodoItem = React.createClass({
-  render: function() {
-    return (
+    
+    render: function() {
+    
+        return (
         <li className={this.props.category + '-ITM'}><input type="checkbox" /> {this.props.thingToDo} <strong><em>{this.props.category}</em></strong></li>
-    );
+        );
   }
 });     
     
