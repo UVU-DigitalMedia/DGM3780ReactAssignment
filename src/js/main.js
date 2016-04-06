8616e7979a62fa8e826458564ebb5970f9287f35
esmitley <10570176@uvlink.uvu.edu>
2016-04-06 10:06:30 -0600

    database setup
    This code is for connecting to a database. It is used in the first
    tutorial not the second. I asked you if it mattered - if it would
    interfere with how the assignment api would function. You said you
    would have to look at it, to put it in git.

--- src/js/main.js
+++ src/js/main.js
@@ -23,31 +23,10 @@ console.error(this.props.url, status, err.toString()); }.bind(this)
 }); },
 handleItemSubmit: function(item) { var items = this.state.data;
 item.id = Date.now();
-var newItems = items.concat([item]); this.setState({data: newItems}); $.ajax({
-url: this.props.url, dataType: 'json',
-type: 'POST',
-data: item,
-success: function(data) {
-this.setState({data: data}); }.bind(this),
-error: function(xhr, status, err) {
-this.setState({data: items});
-console.error(this.props.url, status, err.toString()); }.bind(this)
-}); },
-getInitialState: function() { return {data: []};
-},
-componentDidMount: function() {
-this.loadItemsFromServer();
-setInterval(this.loadItemsFromServer, this.props.pollInterval); },
-render: function() { return (
-<div className="ToDoList">
-<h1>ToDo</h1>
-<ToDoList data={this.state.data} /> <InputBoxonItemSubmit={this.handleItemSubmit} />
-</div> );
-} });
+
 ￼var InputBox = React.createClass({ getInitialState: function() {
 return {author: '', text: ''}; },
-handleAuthorChange: function(e) { this.setState({author: e.target.value});
-},
+
 handleTextChange: function(e) {
 this.setState({text: e.target.value}); },
 handleSubmit: function(e) { e.preventDefault();
@@ -57,15 +36,8 @@ return; }
 this.props.onItemSubmit({author: author, text: text});
 this.setState({author: '', text: ''}); },
 render: function() { return (
-<form className="itemInput" onSubmit={this.handleSubmit}> <input
-type="text"
-placeholder="Job to do" value={this.state.author} onChange={this.handleAuthorChange}
-/> <input
-type="text"
-placeholder="Things tooo do!" value={this.state.text} onChange={this.handleTextChange}
-/>
-<input type="submit" value="Post" /> </form>
-); }
+
+	
 });
 var ItemList = React.createClass({
 render: function() {
@@ -82,9 +54,3 @@ return (
 !
 !
 ! !] });
-var data = [
-! {id:1, author(category?) "!
-! {id:2, author "!
-! {id:3, author "!
-", text:"item"} ", text:"item"}
-", text:"item"}
