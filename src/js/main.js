0326dd1cec16b7942ebf38763d4c0cb30a9ea6d8
esmitley <10570176@uvlink.uvu.edu>
2016-08-19 19:40:15 -0600

    commit two
    added state for UI, handle change, and push, revamped array’s.
    
    I have a few questions about placing values and the submit button ,
    categorySelect vs option, the table css, the check box??? Could you
    give me some feedback.

--- src/js/main.js
+++ src/js/main.js
@@ -13,28 +13,30 @@ var ReactDOM = require('react-dom');
 ///////////////////////////////////////////////////////////////
 var ItemRow = React.createClass ( {
 	render: function (){
-		var name = this.props.item.text
-		 this.rops.item.text:
-		 <span style = {{color:'purple'}}
-	{this.props.item.text}
-	/span>;
+		var item = this.props.item.text
+		 this.props.item.text:
+		 <input type = "checkbox" checked = {this.props.item}
+		   ref = "-----?"
+		   onChange = {this.handleChange} />
+		 <span style = {{color:'purple'}}{this.props.item.text}</span>;
 	return(
 	  <tr
 	    <td {this.props.item.text}/td>
-	  /tr>
+	  </tr>
      );
 	} 
 });
 
-var ItemTable = React.createClass ({
+var ItemListTable = React.createClass ({
 	render: function(){
 		var rows = [];
 		var lastCategory = null;
 		this.props.items.forEach(function(item){
+			return;
 			if(item.category ! == lastCategory){
-				rowspush(<ItemRow item={item} key{item.name}/>);
+				rows.push(<ItemRow item={item} key{item.text}/>);
 						 lastCategory=item.category;
-		});
+		}.bind(this));
 	return(
 	  <table>
 		<tbody>{rows}</tbody>
@@ -43,51 +45,126 @@ var ItemTable = React.createClass ({
 	}
 });
 		
+		
+		
+		
 var item-INPUT = React.createClass({ 
+	handleChange:function(){
+		this.props.onUserInput(
+		this.refs.filterTextInput.value,
+		);
+	};
+	render:function(){
+	  return{item: '', text:''};	    
+});
+		
+		
+		
+var FilteredMultiSelect= require('react-filtered-multiselect')
+	
+var categorySelect = React.createClass({
+	handleChange:function(){
+	this.props.onUserInput(
+	this.refs.filterOption.value,
+		);
+	};
+	render:function(){
+	  return
+	  var selected=(optionState===optionValue "selected")
+	     var options = [
+		  {value:'Long Term'}
+		  {value:'Regular'}
+		  {value:'Urgent'}
+		]
+});
+	
+	 
+var item-Submit = React.createClass({
+	handleChange:function(){
+	this.props.onUserInput(
+	this.refs.filterButton.value,
+	);
+};
+	render:function(){
+		return{button:''};
+	}
+});
+	
+	
+
+var inputTable = React.createClass(
 	render:function(){
 	  return(<form>
 			<p> {''} ToDo Item: </p>
-			<input type = "text" placeholder = "Job Item" />
+			<input type="text" placeholder="Job to do ..." 
+			  value={this.props.filterText}
+			  ref="filterTextInput"
+			  onChange={this.handleChange}/>
+									
 			<p>{''} Category: </p>
-			 <input type = "select" >{this.state.options} />
-			 <input type = "button" {this.state.submit} />
+			 <input type="filteredMultiSelect"> 
+			    value={this.props.filterOption} 
+				ref="filterOption"					
+				onChange={this.handleChange}
+				options={option}/>
+									
+			 <input type="button"> 
+			    value={this.props.filterButton}
+				ref="filterButton"
+				onChange={this.handleChange}/>
 			 </form>
 			);
 	}
-});
-		
-var categorySelect = React.createClass({
-	render:function(){
-	  return(
-	  
-	  )}
-});
-	 
-var item-Submit = React.createClass({
-	render:function(){
-		return
-	}
-});
-		
+);
+	
+	
+	
+	
+	
+	
 var FilterableItemTable = React.createClass({
+	getInitialState
+	return{	
+	filterText: filterText,
+	filterOption: filterOption,
+	filterButton: filterButton
+	
+  };
+},
+	handleUserInput:function(filterText,	){
+		this.setState({
+			filterText: filterText,
+			filterOption: filterOption,
+			filterButton: filterButton
+		});
+     };
+	
 	render:function(){
 		return(
 		 <div>
-			<InputTable />
-			<ItemTable items={this.props.items} />
-		</div>	
-		
-		);
+			<InputTable
+				filterText={this.state.filterText}
+			    filterOption={this.state.filterOption}
+			    filterButton={this.state.filterButton}
+			  onUserInput={this.handleUserInput}/>
+			
+			<ItemListTable 
+				items={this.props.items}
+				 filterText={this.state.filterText}/>
+		</div>
+	  );
 	}
 });
-}
-});
-		
+
+
+
 var ITEMS = [
-	{category:'urgent-ITM', item:'text', value:'1'},
-	{category:'regular-ITM', item:'text', value:'2'},
-	{category:'long-term-ITM', item:'text', value:'3'}
+	{category:'urgent', value:1, text: 'Item One', item:'text'},
+	{category:'regular', value:2, text: 'Item Two', item:'text'},
+	{category:'long-term', value:3, text: 'Item Three', item:'text'}
 ]
+
+
 
 ReactDOM.render(
   <FilterableItemTable items = {ITEMS}/>
